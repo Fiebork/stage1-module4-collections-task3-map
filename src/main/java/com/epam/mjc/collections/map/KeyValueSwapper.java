8776e80433a0fcc7ec474e7306ffba9a1392e4a1
Bilbo3340 <vladik.elce1@gmail.com>
2022-09-03 15:18:27 +0300

    upt

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,28 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map <String, Integer> map = new HashMap<>();
+        for (int i = 0; i <= sourceMap.size(); i++) {
+            Integer min = null;
+            for (Integer elem : sourceMap.keySet()) {
+                if (Objects.equals(sourceMap.get(elem), sourceMap.get(i))) {
+                    if (min == null) {
+                        min = elem;
+                    } else if (elem <= min) {
+                        min = elem;
+                    }
+                }
+            }
+            if (min != null) {
+                map.put(sourceMap.get(i), min);
+            }
+        }
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,26 @@
 package com.epam.mjc.collections.map;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] subStr;
+        subStr = sentence.toLowerCase().split("[\\s,.!?:']");
+        for (int i = 0; i < subStr.length; i++) {
+            if (!map.containsKey(subStr[i]) && !Objects.equals(subStr[i], "")) {
+                int sum = 1;
+                for (int j = i + 1; j < subStr.length; j++) {
+                    if (Objects.equals(subStr[i], subStr[j])) {
+                        sum++;
+                    }
+                }
+                map.put(subStr[i], sum);
+            }
+        }
+        return map;
     }
 }
